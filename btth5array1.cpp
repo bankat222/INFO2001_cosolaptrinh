a7f0fd6fedd378b36a35495a28d8820d29846ced
bankat222 <146832000+bankat222@users.noreply.github.com>
2023-11-28 20:49:30 +0700

    btth5array1.cpp

--- btth5array1.cpp
+++ btth5array1.cpp
@@ -1,34 +1,61 @@
 #include <stdio.h>
-#include <stdlib.h>
-void bubblesort(int arr[], int n){
-	for(int i=0; i<n-1; i++){
-	for(int j= 0; j<n; j++){
-	  	if( arr[i]>arr[j])
-	  		int block= arr[i];
-	  		arr[i]= arr[i+1];
-	}
-  }
+#include <time.h>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
 }
-void selectionsort(int arr[], int n){
-	int i, j, min_idx;
-	for(int i=0; i<n-1; i++){
-		min_idx=i;
-	for(int j=i+1; j<n; j++){
-		if(arr[j]<arr[min_idx])
-		   min_idx= j;
-			int block= arr[min_idx];
-			arr[min_idx]=arr[i];
-			arr[i]=block;
-		}
-	}	
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        int min_idx = i;
+        for (int j = i+1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
 }
-int main(){
-	int a, b;
-	printf("Nhap hieu suat trong tung mang: \n");
-	printf("Nhap hieu suat cho bubblesort: ");
-	scanf("%d", &a);
-	printf("Nhap hieu suat cho selectionsort: ");
-	scanf("%d", &b);
-	printf("Thoi gian thuc thi la:%d %d\n", bubblesort, selectionsort);
-	return 0;
+void printArray(const int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+int main() {
+    int n;
+    printf("Nhap so phan tu: ");
+    scanf("%d", &n);
+
+    int arrBubble[n], arrSelection[n];
+
+    printf("Nhap cac phan tu: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arrBubble[i]);
+        arrSelection[i] = arrBubble[i];
+    }
+    clock_t start_bubble = clock();
+    bubbleSort(arrBubble, n);
+    clock_t end_bubble = clock();
+
+    printf("Mang sau Bubble Sort: ");
+    printArray(arrBubble, n);
+    printf("Thoi gian Bubble Sort: %.6f seconds\n", (double)(end_bubble - start_bubble) / CLOCKS_PER_SEC);
+
+    clock_t start_selection = clock();
+    selectionSort(arrSelection, n);
+    clock_t end_selection = clock();
+
+    printf("\nMang sau Selection Sort: ");
+    printArray(arrSelection, n);
+    printf("Thoi gian Selection Sort: %.6f seconds\n", (double)(end_selection - start_selection) / CLOCKS_PER_SEC);
+
+    return 0;
 }
